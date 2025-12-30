f5852f8edc28119169a7fd939cd96731b8221541
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2025-12-30 17:08:34 +0300

    new structure

--- hotel/includes/checklogin.php
+++
@@ -1,8 +0,0 @@
-<?php 
-function check_login() {
-    if(strlen($_SESSION['id']) == 0) {
-        $_SESSION['id']="";
-        header("Location: logout.php");
-    }
-}
-?>
--- hotel/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host="localhost";
-$dbuser="root";
-$dbpass="";
-$db="hotel";
-$mysqli = new mysqli($host, $dbuser, $dbpass, $db);
-?>
