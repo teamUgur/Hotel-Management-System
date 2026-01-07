1521ae87c2ce92ff970c152ba9259c823589aa46
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-01-07 17:32:09 +0300

    updated js

--- js/index.js
+++ js/index.js
@@ -12,18 +12,8 @@ closeBtn.addEventListener("click", function() {
     bookBox.style.display = "none";
 })
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const bookBox = document.getElementById('guestdetailpanel');
-//     const bookBtns = document.querySelectorAll(".bookbtn");
-//     const closeBtn = document.getElementById("closeBtn");
-
-//     bookBtns.forEach(button => {
-//         button.addEventListener("click", () => {
-//             bookBox.style.display = "flex";
-//         });
-//     });
-
-//     closeBtn.addEventListener("click", function() {
-//         bookBox.style.display = "none";
-//     });
-// });
+bookBox.addEventListener("click", function(event) {
+    if (event.target === bookBox) {
+        bookBox.style.display = "none";
+    }
+})
