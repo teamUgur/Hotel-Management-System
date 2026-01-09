d35a648316058a01e36c995edab13ef7cfb5060e
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-01-09 22:42:41 +0300

    booked room chart

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -39,7 +39,7 @@ $roomtypeFourRow = mysqli_num_rows($roomtypeFourSql);
 $incomeSql = 'SELECT * FROM payment'; 
 $result = mysqli_query($conn, $incomeSql);
 $total = 0;
-$char_data = "";
+$chart_data = "";
 
 while($row = mysqli_fetch_array($result)) {
     $chart_data .= "{ date:'".$row["cout"]."', profit:".$row["finaltotal"]*10/100 ."}, ";
@@ -48,3 +48,88 @@ while($row = mysqli_fetch_array($result)) {
 
 $chart_data = substr($chart_data, 0, -2);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dasboard</title>
+    <link rel="stylesheet" href="./css/dashboard.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+    <div id="data-box">
+
+        <div class="box room-book-box">
+            <h2>Total Booked Room</h2>
+            <h1><?php echo $roombookRow ?> / <?php echo $roomRow ?></h1>
+        </div>
+
+        <div class="box staff-box">
+            <h2>Total Staff</h2>
+            <h1><?php echo $staffRow?></h1>
+        </div>
+
+        <div class="box profit-box">
+            <h2>Profit</h2>
+            <h1><?php echo $total?> <span>&#8381;</span></h1>
+        </div>
+        
+    </div>
+
+    <div id="charts-box">
+
+        <div class="booked-room-chart">
+            <canvas id="booked-room-canvas"></canvas>
+            <h3>Booked Room</h3>
+        </div>
+
+        <div class="profit-bars">
+            <canvas id="profit-canvas"></canvas>
+            <h3>Profit</h3>
+        </div>
+
+    </div>
+</body>
+
+<!-- BOOKED ROOMS CHART -->
+<script>
+    const labels = [
+          'Superior Room',
+          'Deluxe Room',
+          'Guest House',
+          'Single Room',
+        ];
+      
+        const data = {
+          labels: labels,
+          datasets: [{
+            label: 'My First dataset',
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(153, 102, 255, 1)',
+            ],
+            borderColor: 'black',
+            data: [<?php echo $roomtypeOneRow ?>,<?php echo $roomtypeTwoRow ?>,<?php echo $roomtypeThreeRow  ?>,<?php echo $roomtypeFourRow ?>],
+          }]
+        };
+  
+        const doughnutchart = {
+          type: 'doughnut',
+          data: data,
+          options: {}
+        };
+        
+      const myChart = new Chart(
+      document.getElementById('booked-room-canvas'),
+      doughnutchart);
+
+</script>
+
+<script>
+
+</script>
+</html>
