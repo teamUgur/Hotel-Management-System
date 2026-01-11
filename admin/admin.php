8e920921d0b364049a5e38dfda30a9b129ab011c
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-01-11 11:46:47 +0300

    flash.css

--- admin/admin.php
+++ admin/admin.php
@@ -21,6 +21,8 @@ if ($usermail == true) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link rel="stylesheet" href="./css/admin.css">
+    <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+    <link rel="stylesheet" href="../css/flash.css">
 </head>
 <body>
 
