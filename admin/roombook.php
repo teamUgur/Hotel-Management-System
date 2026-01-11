16610782d42319e9d5f2ef2f2621857297521b52
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-01-11 22:01:57 +0300

    update roombook

--- admin/roombook.php
+++ admin/roombook.php
@@ -23,80 +23,98 @@ include "../config.php"
 
     <div id="guestdetailpanel">
         <form action="" method="POST" class="guestdetailpanelform">
-            <div class="head">
-                <h3>RESERVATION</h3>
-                <i class="fa-solid fa-circle-xmark" onclick="adduserclose()"></i>
-            </div>
-            <div class="middle">
-                <div class="guestinfo">
-                    <h4>Guest information</h4>
-                    <input type="text" name="Name" placeholder="Enter Full name" required>
-                    <input type="email" name="Email" placeholder="Enter Email" required>
 
+            <div class="header-part">
+                <h3>Reservation</h3>
+                <i class="fa-solid fa-circle-xmark" id="closeBtn"></i>
+            </div>
+
+            <div class="middle-part">
+
+                <!-- FIRST COL -->
+
+                <div class="guest-info">
+                    <h4>Guest Information</h4>
+                    <input type="text" name="Name" placeholder="Enter your full name">
+                    <input type="email" name="Email" placeholder="Enter your email">
+                    
                     <?php
                     $countries = array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
                     ?>
 
-                    <select name="Country" class="selectinput" required>
-						<option value selected >Select your country</option>
+                    <select name="Country" class="selectinput">
+
+                        <option value selected>Select your Country</option>
+                    
                         <?php
-							foreach($countries as $key => $value):
-							echo '<option value="'.$value.'">'.$value.'</option>';
-                            //close your tags!!
-							endforeach;
-						?>
+                        foreach($countries as $key => $value):
+                            echo '<option value="'.$value.'">'.$value.'</option>';
+                            endforeach;
+                        ?>
+
                     </select>
-                    <input type="text" name="Phone" placeholder="Enter Phoneno" required>
+                    <input type="text" name="Phone" placeholder="Enter your phone number">
                 </div>
+                
 
                 <div class="line"></div>
 
-                <div class="reservationinfo">
+                <!-- SECOND COL -->
+
+                <div class="reservation-info">
                     <h4>Reservation information</h4>
-                    <select name="RoomType" class="selectinput">
-						<option value selected >Type Of Room</option>
-                        <option value="Superior Room">SUPERIOR ROOM</option>
-                        <option value="Deluxe Room">DELUXE ROOM</option>
-						<option value="Guest House">GUEST HOUSE</option>
-						<option value="Single Room">SINGLE ROOM</option>
+
+                    <select name="RoomType" id="type-of-room">
+                        <option value selected>Type of Room</option>
+                        <option value="Superior room">Superior room</option>
+                        <option value="Deluxe room">Deluxe room</option>
+                        <option value="Guest house">Guest house</option>
+                        <option value="Single room">Single room</option>
                     </select>
-                    <select name="Bed" class="selectinput">
-						<option value selected >Bedding Type</option>
-                        <option value="Single">Single</option>
-                        <option value="Double">Double</option>
-						<option value="Triple">Triple</option>
-                        <option value="Quad">Quad</option>
-						<option value="None">None</option>
+
+                    <select name="Bed" id="bedding-type">
+                        <option value selected>Begging Type</option>
+                        <option value="single">Single</option>
+                        <option value="double">Double</option>
+                        <option value="triple">Triple</option>
+                        <option value="quad">Quad</option>
+                        <option value="none">None</option>
                     </select>
-                    <select name="NoofRoom" class="selectinput">
-						<option value selected >No of Room</option>
+
+                    <select name="NoofRoom" id="number-of-room">
+                        <option value selected>Number of room</option>
                         <option value="1">1</option>
                     </select>
-                    <select name="Meal" class="selectinput">
-						<option value selected >Meal</option>
-                        <option value="Room only">Room only</option>
-                        <option value="Breakfast">Breakfast</option>
-						<option value="Half Board">Half Board</option>
-						<option value="Full Board">Full Board</option>
-					</select>
-                    <div class="datesection">
-                        <span>
-                            <label for="cin"> Check-In</label>
-                            <input name="cin" type ="date">
+
+                    <select name="Meal" id="meal">
+                        <option value selected>Meal</option>
+                        <option value="room-only">Room Only</option>
+                        <option value="breakfast">Breakfast</option>
+                        <option value="half-board">Half Board</option>
+                        <option value="full-board">Full Board</option>
+                    </select>
+
+                    <div class="check-in-out">
+                        <span id="check-in">
+                            <label for="cin">Check In</label>
+                            <input name="cin" type="date">
                         </span>
-                        <span>
-                            <label for="cin"> Check-Out</label>
-                            <input name="cout" type ="date">
+
+                        <span id="check-out">
+                            <label for="cout">Check Out</label>
+                            <input name="cout" type="date">
                         </span>
                     </div>
                 </div>
             </div>
+
             <div class="footer">
                 <button class="btn btn-success" name="guestdetailsubmit">Submit</button>
             </div>
         </form>
+    </div>
 
-        <!-- room availablity start-->
+        <!-- room availablity -->
 
         <?php
 
@@ -185,5 +203,7 @@ include "../config.php"
             $availableFive = "No";
         }
         ?>
+
+        
 </body>
 </html>
